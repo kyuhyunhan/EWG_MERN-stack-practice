ba8ca511eef8e347f023fa71c25bb98435fd21dd
kyuhyun <kyuhyunhaan@gmail.com>
2020-11-04 10:32:40 +0900

    fixed API connecting issue. (mismatched url)

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 require('dotenv').config()
+//const cors = require('cors')
 const fetchRouter = require('./src/routes/fetch')
 
 const app = express()
 const port = process.env.PORT || 3001;
 
+//app.use(cors())
 app.use('/', fetchRouter)
 
 app.listen(port, function() {
--- server/src/routes/fetch.js
+++ server/src/routes/fetch.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const axios = require('axios');
+const axios = require('axios').default;
 
 router.route('/').get(async function(req,res){
-    const url = 'https://wordsapiv1.p.mashape.com/words?random=true'
+    console.log('just connected to backend')
+    const url = 'https://rapidapi.p.rapidapi.com/words/?random=true'
     const headers = {
         'content-type':'application/json',
         'x-rapidapi-key': process.env.WORDSAPI_KEY,
@@ -11,7 +12,7 @@ router.route('/').get(async function(req,res){
     }
 
     try {
-        const response = await axios ({
+        const response = await axios.request({
             method: "GET",
             url: url,
             headers: headers
@@ -21,6 +22,21 @@ router.route('/').get(async function(req,res){
     } catch {
         console.log('e')
     }
+    // var options = {
+    //     method: 'GET',
+    //     url: 'https://rapidapi.p.rapidapi.com/words/?random=true',
+    //     headers: {
+    //         // 'content-type':'application/json',
+    //         'x-rapidapi-key': process.env.WORDSAPI_KEY,
+    //         'x-rapidapi-host': 'wordsapiv1.p.rapidapi.com'
+    //         // 'accept':'application/json'
+    //     }
+    // }
+    // axios.request(options).then(function(response){
+    //     console.log(response.data)
+    // }).catch (function(error){
+    //     console.error(error)
+    // })
 })
 
 module.exports = router
