760158cd77051d12eb12d2480a53091925e59e1a
kyuhyun <kyuhyunhaan@gmail.com>
2020-11-09 13:41:57 +0900

    returned to the first basic form

--- client/src/components/Givenword.js
+++ client/src/components/Givenword.js
@@ -6,25 +6,13 @@ class Givenword extends React.Component {
     constructor (props){
         super(props);
         this.state = {
-            word: '',
-            FisSynonym: false,
-            SisSynonym: false
-            
+            word: ''
         }
     }
     componentDidMount() {
         axios.get('http://localhost:3001/api/random')
             .then((response) => {
                 this.setState({ word: response.data })
-                // var random = Math.random()
-                // if(random > 0.5){
-                //     this.setState({FisSynonym: true})
-                //     this.setState({SisSynonym: false})
-                // } else {
-                //     this.setState({FisSynonym: false})
-                //     this.setState({SisSynonym: true})
-                // }
-                console.log(this.state.word + '/ ' + this.state.FisSynonym + ', ' + this.state.SisSynonym)
             })
             .catch(function(error){
                 console.log(error)
